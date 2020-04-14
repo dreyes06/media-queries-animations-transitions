8878678a1bd4a14fc48a32e3c80cffe5865cf013
Daniel <djreyes042497@gmail.com>
2020-04-13 19:00:11 -0500

    solutions

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export default props => (
     <div className='body-component'>
         <div className='rotating-box'>Make me rotate!</div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,18 +4,42 @@ class Header extends Component {
     constructor(props){
         super(props);
         //Dropdown needs a value to work with conditional rendering
+        this.state = {
+            dropdownView: false
+        }
+    }
+
+     handleToggle = () => {
+        this.setState({dropdownView: !this.state.dropdownView})
     }
 
     render(){
         return (
             <div className='header'>
                 <img src='https://s3.amazonaws.com/media-p.slid.es/uploads/75809/images/1730601/DevMtnLogo.png' alt='devmountain' className='header-logo'/>
-                <div className='dropdown-button'>
+                <div className='dropdown-button' onClick={this.handleToggle} >
                     Menu
                 </div>
+                {
+                this.state.dropdownView
+                ? (
+                    <nav className='dropdown-menu' >
+                    <span>Home</span>
+                    <span>About</span>
+                    <span>Contact</span>
+                    </nav>
+                )
+                : 
+                null
+                }
+                <nav className='desktop-links' >
+                    <span>Home</span>
+                    <span>About</span>
+                    <span>Contact</span>
+                </nav>
             </div>
         )
     }
 }
 
-export default Header;
+export default Header; 
